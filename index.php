1d7ab05411fa2cafed4ac85c14a68d6b91205b03
roomber-dev <roomberapp@gmail.com>
2021-08-31 21:30:27 +0300

    Tidied up website title

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Roomber cutting-edge</title>
     <link rel="stylesheet" href="style.css">
     <script src="messages.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
