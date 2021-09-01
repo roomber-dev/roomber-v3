5a14b3f23831493ac870d28271b2cd804d02dfd0
NixxdeOfficial <64833558+NixxdeOfficial@users.noreply.github.com>
2021-09-01 10:45:58 +0200

    array message update + message statuses

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,6 +1,20 @@
-let message = (username, date = new Date(), text, id) => {
+let messages = []
 
-	
+
+function sendMessage(username, date = new Date(), text, id) {
+	let json = {
+		date: date,
+		text: text,
+		username: username,
+		id: id,
+		status: -1
+	}
+	messages.push(json);
+}
+
+let message = (username, date = new Date(), text, id, status) => {
+
+
 	var day = date.getDate().toString().padStart(2, "0");
 	var mon = date.getMonth().toString().padStart(2, "0");
 	var yer = date.getFullYear();
@@ -8,13 +22,22 @@ let message = (username, date = new Date(), text, id) => {
 	var min = date.getMinutes().toString().padStart(2, "0");
 
 	var timestamp = `${day}/${mon}/${yer} at ${hour}:${min}`;
+var statusClass;
+	if(status = -1) {
+		statusClass = "failed";
+	} else if(status = 0) {
+		statusClass = "sending";
+	} else if(status = 1) {
+		statusClass = "";
+	}
+
 	return `<div class="message glass" id="${id}">
 		<div class="flex">
 		    <img src="avatars/default.png" class="avatar">
 		    <div class="flex">
 		        <div class="flex-down">
 		            <div class="username">${username}</div>
-		            <div class="msgln">
+		            <div class="msgln ${statusClass}">
 		                ${text}
 		            </div>
 		        </div>
@@ -28,24 +51,75 @@ let msgCount = 0;
 
 $("#submitmsg").click(() => {
 	let id = "msg" + (msgCount += 1)
-	chat.innerHTML += message(
+	/*chat.innerHTML += message(
 		"someever for now", 
 		new Date(), 
 		$("#usermsg").val(),
 		id
-	);
+	);*/
 
-	let messageElement = $("#" + id)[0];
+	/*message(
+		"someever for now", 
+		new Date(), 
+		$("#usermsg").val(),
+		id
+	);*/
 
-	for(var i = 1; i <= msgCount; i++) {
-		let element = $("#msg" + i)[0];
-		element.style = "";
+	sendMessage(
+		"someever for now",
+		new Date(),
+		$("#usermsg").val(),
+		id
+	)
+
+	updateChat();
+
+
+
+	let messageElement = $("#" + id);
+
+	for (var i = 1; i <= msgCount; i++) {
+		let element = $("#msg" + i);
+		if (element) {
+			element.css("", "");
+		}
 	}
 
-	messageElement.style = "animation: message-enter 1s";
+	
 
 	return false;
 });
+
+function updateChat() {
+	let htmlstring = "";
+
+	messages.forEach(element => {
+		return htmlstring += message(
+			element.username,
+			element.date,
+			element.text,
+			element.id,
+			element.status
+		)
+
+
+
+	});
+
+	
+
+
+	$("#chat").html(htmlstring);
+	$("#"+messages[messages.length - 1].id).css("animation", "message-enter 1s")
+
+	for (var i = 1; i <= msgCount; i++) {
+		let element = $("#msg" + i);
+		if (element) {
+			element.css("", "");
+		}
+	}
+	
+}
 
 
 
